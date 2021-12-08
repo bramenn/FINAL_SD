262c7c70f0e7e5a26d0001441de8542db5377b53
Alejandro Herrera <brayan.herrera@utp.edu.co>
2021-12-07 21:51:37 -0500

    Se crean las relaciones entre las tablas -- se crea la ruta obetner_votantes y su consulta en bd, igual para obtener_votante/<cedula>

---
+++ Votante/consultas.py
@@ -0,0 +1,28 @@
+from Votante.modelo import Votante, Votante_apoyo
+import db
+
+def obtener_votantes():
+    """
+    Obtener todos los votantes
+    query:
+        select * from votante;
+    """
+    votante = db.session.query(Votante).all()
+    return votante
+
+def obtener_votante_por_cedula(cedula:str):
+    votante = db.session.query(Votante).where(Votante.cedula==cedula).first()
+
+    if not votante:
+        return None
+
+    votante_dict = {
+            "cedula": votante.cedula,
+            "nombre": votante.nombre,
+            "apellidos": votante.apellidos,
+            "email": votante.email,
+            "celular": votante.celular,
+            "fotografia": votante.fotografia,
+            "password": votante.password,
+        }
+    return votante_dict
--- Votante/modelo.py
+++ Votante/modelo.py
@@ -1,3 +1,4 @@
+from sqlalchemy.orm import relationship
 import db
 from sqlalchemy import Column, Integer, Boolean, String
 from pydantic import BaseModel
@@ -6,14 +7,14 @@ from pydantic import BaseModel
 class Votante(db.Base):
     """Este modelo define los atributos de la tabla votante y sus tipos de dato"""
     __tablename__ = "votante"
-    cedula = Column("cedula", Integer, primary_key=True, unique=True, index=True)
+    cedula = Column("cedula", String(255), primary_key=True, unique=True, index=True)
     nombre = Column("nombre", String(255))
     apellidos = Column("apellidos", String(255))
     email = Column("email", String(255))
     celular = Column("celular", String(255))
     fotografia = Column("fotografia", String(255))
     password = Column("password", String(255))
-
+    voto = relationship("Voto", back_populates="votante", uselist=False)
 
 ## MODELO PARA RECIBIR INFORMACION DE UNA PETICION
 class Votante_apoyo(BaseModel):
@@ -23,5 +24,4 @@ class Votante_apoyo(BaseModel):
     email: str
     celular: str
     fotografia: str
-
-    
+    password: str
